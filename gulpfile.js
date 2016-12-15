11695451c17710bf81e1b6650afff872d2f1ab21
Stuart Yamartino <stuart@yamartino.com>
2016-12-14 22:15:20 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var inlineCss = require('gulp-inline-css');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/email.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('sass:build', function () {
+  gulp.run('sass');
+});
+
+gulp.task('examples:button', function() {
+  return gulp.src('./examples/preinlined/*.html')
+    .pipe(inlineCss({
+      applyStyleTags: false,
+      applyLinkTags: true,
+      removeStyleTags: false,
+      removeLinkTags: true
+    }))
+    .pipe(gulp.dest('./examples/inlined/'));
+});
