101bd04f5f824a7072e027c354fe182224815aec
Stuart Yamartino <stuart@yamartino.com>
2017-08-16 22:00:05 -0400

    new boostrap html syntax replaces with table via cheerio and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inlineCss = require('gulp-inline-css');
+var cheerio = require('gulp-cheerio');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/email.scss')
@@ -12,15 +13,8 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/*.scss', ['sass']);
 });
 
-gulp.task('sass:build', function () {
-  gulp.run('sass');
-});
-
 gulp.task('examples', function() {
   return gulp.src('./examples/preinlined/*.html')
-    .pipe(function(text){
-      console.log(text)
-    })
     .pipe(inlineCss({
       applyStyleTags: false,
       applyLinkTags: true,
@@ -30,3 +24,22 @@ gulp.task('examples', function() {
     }))
     .pipe(gulp.dest('./examples/inlined/'));
 });
+
+gulp.task('cheerio', function() {
+  return gulp.src('./examples/preinlined/*.html')
+    .pipe(cheerio(function ($, file) {
+      $('.btn').each(function(){
+        $(this).replaceWith($('<table class="'+$(this).attr('class')+'" align="left"><tr><td>'+$.html($(this).removeAttr('class'))+'</td></tr></table>'));
+      });
+    }))
+    .pipe(inlineCss({
+      applyStyleTags: false,
+      applyLinkTags: true,
+      removeStyleTags: false,
+      removeLinkTags: true,
+      applyTableAttributes: true
+    }))
+    .pipe(gulp.dest('./examples/inlined/'));
+});
+
+gulp.task('build', ['sass', 'cheerio']);
