6e9162aae32415233f66a2844d5fe5a5e7a10cac
Stuart Yamartino <stuart@yamartino.com>
2017-09-18 00:36:44 -0400

    I don't like that cheerio is giving me issues and I need to write the margin selector out

--- gulpfile.js
+++ gulpfile.js
@@ -42,31 +42,31 @@ gulp.task('cheerio', function() {
     .pipe(wrap({ src: 'template.html'}))
     .pipe(styleInject())
     .pipe(cheerio(function ($, file) {
-      $('.btn').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeClass('btn'))});
+      $('.btn').each(function(){ // move all classes up and remove all classes from element
+        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
-      $('.badge').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeClass('badge'))});
+      $('.badge').each(function(){ // move all classes up and remove all classes from element
+        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('badge'))});
       });
-      $('.alert').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeClass('alert'))});
+      $('.alert').each(function(){ // move all classes up and remove all classes from element
+        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('alert'))});
       });
-      $('.align-left').each(function(){
+      $('.align-left').each(function(){ // align table and move contents
         buildFromTemplate.call(this, $, 'align-left', {contents: $.html($(this))});
       });
-      $('.align-center').each(function(){
+      $('.align-center').each(function(){ // align table and move contents
         buildFromTemplate.call(this, $, 'align-center', {contents: $.html($(this))});
       });
-      $('.align-right').each(function(){
+      $('.align-right').each(function(){ // align table and move contents
         buildFromTemplate.call(this, $, 'align-right', {contents: $.html($(this))});
       });
-      $('.card').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeClass('card'))});
+      $('.card').each(function(){ // move all classes up and remove all classes from element
+        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('card'))});
       });
-      $('.card-body').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeClass('card-body'))});
+      $('.card-body').each(function(){ // move all classes up and remove all classes from element
+        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('card-body'))});
       });
-      $('hr').each(function(){
+      $('hr').each(function(){ // drop hr in place of current
         buildFromTemplate.call(this, $, 'hr', {});
       });
       $('p').each(function(){
@@ -84,10 +84,11 @@ gulp.task('cheerio', function() {
       $('*[class^=col]').each(function(){
         buildFromTemplate.call(this, $, 'col', {classes: $(this).attr('class'), contents: $(this).html()});
       });
-      $('*[class^=m-],*[class^=mt-],*[class^=mr-],*[class^=mb-],*[class^=ml-],*[class^=mx-],*[class^=my-]').each(function(){
+      $('.m-1,.m-2,.m-3,.m-4,.m-5,.mt-1,.mt-2,.mt-3,.mt-4,.mt-5,.mr-1,.mr-2,.mr-3,.mr-4,.mr-5,.mb-1,.mb-2,.mb-3,.mb-4,.mb-5,.ml-1,.ml-2,.ml-3,.ml-4,.ml-5,.mx-1,.mx-2,.mx-3,.mx-4,.mx-5,.my-1,.my-2,.my-3,.my-4,.my-5').each(function(){
         // make margins into a new empty table and make it a padding class
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class').replace(/m([trblxy]?-\d)/g, 'p$1').match(/p[trblxy]?-\d/g), contents: $.html($(this))});
+        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class').replace(/m([trblxy]?-\d)/g, "p$1").match(/p[trblxy]?-\d/g), contents: $.html($(this))});
       });
+
     }))
     .pipe(inlineCss({
       applyStyleTags: false,
