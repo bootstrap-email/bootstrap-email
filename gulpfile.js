f1dce00bd28dd8b996a9330ca28911cd692c82b9
Stuart Yamartino <stuart@yamartino.com>
2017-09-26 23:43:35 -0400

    adds margin div helper and always use thead and tbody

--- gulpfile.js
+++ gulpfile.js
@@ -43,13 +43,13 @@ gulp.task('cheerio', function() {
     .pipe(styleInject())
     .pipe(cheerio(function ($, file) {
       $('.btn').each(function(){ // move all classes up and remove all classes from element
-        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
+        buildFromTemplate.call(this, $, 'table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
       $('.badge').each(function(){ // move all classes up and remove all classes from element
-        buildFromTemplate.call(this, $, 'basic-table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
+        buildFromTemplate.call(this, $, 'table-left', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
       $('.alert').each(function(){ // move all classes up and remove all classes from element
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
+        buildFromTemplate.call(this, $, 'table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
       $('.align-left').each(function(){ // align table and move contents
         buildFromTemplate.call(this, $, 'align-left', {contents: $.html($(this))});
@@ -61,22 +61,22 @@ gulp.task('cheerio', function() {
         buildFromTemplate.call(this, $, 'align-right', {contents: $.html($(this))});
       });
       $('.card').each(function(){ // move all classes up and remove all classes from element
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
+        buildFromTemplate.call(this, $, 'table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
       $('.card-body').each(function(){ // move all classes up and remove all classes from element
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
+        buildFromTemplate.call(this, $, 'table', {classes: $(this).attr('class'), contents: $.html($(this).removeAttr('class'))});
       });
       $('hr').each(function(){ // drop hr in place of current
         buildFromTemplate.call(this, $, 'hr', {});
       });
       $('p').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table', {classes: 'p', contents: $.html($(this))});
+        buildFromTemplate.call(this, $, 'table', {classes: 'p', contents: $.html($(this))});
       });
       $('.container').each(function(){
         buildFromTemplate.call(this, $, 'container', {classes: $(this).attr('class'), contents: $(this).html()});
       });
       $('.container-fluid').each(function(){
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $(this).html()});
+        buildFromTemplate.call(this, $, 'table', {classes: $(this).attr('class'), contents: $(this).html()});
       });
       $('.row').each(function(){
         buildFromTemplate.call(this, $, 'row', {classes: $(this).attr('class'), contents: $(this).html()});
@@ -85,14 +85,21 @@ gulp.task('cheerio', function() {
         buildFromTemplate.call(this, $, 'col', {classes: $(this).attr('class'), contents: $(this).html()});
       });
       $('*[class^=p-]').each(function(){
+        console.log('made it');
         if($(this).tagName != 'table'){
-          buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class'), contents: $.html($(this))});
+          buildFromTemplate.call(this, $, 'table', {classes: $(this).attr('class'), contents: $.html($(this))});
         }
-      })
-      $('.m-1,.m-2,.m-3,.m-4,.m-5,.mt-1,.mt-2,.mt-3,.mt-4,.mt-5,.mr-1,.mr-2,.mr-3,.mr-4,.mr-5,.mb-1,.mb-2,.mb-3,.mb-4,.mb-5,.ml-1,.ml-2,.ml-3,.ml-4,.ml-5,.mx-1,.mx-2,.mx-3,.mx-4,.mx-5,.my-1,.my-2,.my-3,.my-4,.my-5').each(function(){
-        // make margins into a new empty table and make it a padding class
-        buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class').replace(/m([trblxy]?-\d)/g, "p$1").match(/p[trblxy]?-\d/g).join(' '), contents: $.html($(this))});
       });
+      $('.m-1,.m-2,.m-3,.m-4,.m-5,.mt-1,.mt-2,.mt-3,.mt-4,.mt-5,.mr-1,.mr-2,.mr-3,.mr-4,.mr-5,.mb-1,.mb-2,.mb-3,.mb-4,.mb-5,.ml-1,.ml-2,.ml-3,.ml-4,.ml-5,.mx-1,.mx-2,.mx-3,.mx-4,.mx-5,.my-1,.my-2,.my-3,.my-4,.my-5').each(function(){
+        console.log('made it 2222222');
+        if($(this).tagName != 'div'){
+          buildFromTemplate.call(this, $, 'div', {classes: $(this).attr('class').match(/m([trblxy]?-\d)/g).join(' '), contents: $.html($(this))});
+        }
+      });
+      // $('.m-1,.m-2,.m-3,.m-4,.m-5,.mt-1,.mt-2,.mt-3,.mt-4,.mt-5,.mr-1,.mr-2,.mr-3,.mr-4,.mr-5,.mb-1,.mb-2,.mb-3,.mb-4,.mb-5,.ml-1,.ml-2,.ml-3,.ml-4,.ml-5,.mx-1,.mx-2,.mx-3,.mx-4,.mx-5,.my-1,.my-2,.my-3,.my-4,.my-5').each(function(){
+      //   // make margins into a new empty table and make it a padding class
+      //   buildFromTemplate.call(this, $, 'basic-table', {classes: $(this).attr('class').replace(/m([trblxy]?-\d)/g, "p$1").match(/p[trblxy]?-\d/g).join(' '), contents: $.html($(this))});
+      // });
 
     }))
     .pipe(inlineCss({
--- readme.md
+++ readme.md
@@ -29,7 +29,8 @@ This project is still under development and looking for contributors willing to 
 
 ## Email Quirks (internal notes)
 - Line height should always be in px never a number or percentage. https://www.marketingcloud.com/blog/design-tip-of-the-week-css-line-height-property-does-it-work-in-email/
-- The only way to add spacing is padding inside of a table cell.
+- Padding can only be used inside of a table cell.
+- Margin can only be used on divs.
 - Font family is reset at the top of every new table.
 - Responsive media query for stacking table cells with display block only works on Android in table header `<th>` cells and not table cells `<td>`.
 - To make a table width 100% BOTH the table and the td tags must be set to 100%
