ec0d92ef8cbde5e78562b38d9112eede0118d518
Stuart <stuart@yamartino.com>
2020-07-14 22:05:55 -0400

    lint

--- lib/bootstrap-email/action_mailer.rb
+++ lib/bootstrap-email/action_mailer.rb
@@ -1,11 +1,11 @@
 class ActionMailer::Base
   # sit in the middle and compile the html
-  def bootstrap_mail *args
+  def bootstrap_mail(*args)
     bootstrap = BootstrapEmail::Compiler.new(mail(*args) { |format| format.html { render layout: 'layouts/bootstrap-mailer.html.erb' } })
     bootstrap.perform_full_compile
   end
 
-  def make_bootstrap_mail *args, &block
+  def make_bootstrap_mail(*args, &block)
     bootstrap = BootstrapEmail::Compiler.new(mail(*args, &block))
     bootstrap.perform_full_compile
   end
