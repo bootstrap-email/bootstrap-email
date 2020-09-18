07dd9bf9201592e9d967dbd4ee12253276bf987e
Stuart <stuart@yamartino.com>
2020-09-18 19:58:12 -0400

    Move test file

--- tests/run.rb
+++ tests/run.rb
@@ -1,20 +1,22 @@
-require_relative 'lib/bootstrap-email'
+require_relative '../lib/bootstrap-email'
 require 'sassc'
 
 def embed_in_layout(html)
   namespace = OpenStruct.new(contents: html)
-  template_html = File.read(File.expand_path('tests/layout.html.erb', __dir__))
+  template_html = File.read(File.expand_path('layout.html.erb', __dir__))
   ERB.new(template_html).result(namespace.instance_eval { binding })
 end
 
 def run_tests
+  puts 'Starting tests...'
   Dir.glob('tests/precompiled/*.html').each do |file|
     file_contents = File.read(file)
     compiled = BootstrapEmail::Compiler.new(type: :string, input: embed_in_layout(file_contents)).perform_full_compile
     destination = file.split('/').last
     File.write("tests/compiled/#{destination}", compiled)
-    puts "Compiled file #{destination}"
+    puts "Compiled #{destination}"
   end
+  puts 'Finished compiling test files!'
 end
 
 run_tests
