45c7f9e4d27fe6dc3aa3a978410dd3ec9f7d4afd
Stuart <stuart@yamartino.com>
2020-09-20 17:25:11 -0400

    Cuter log lines for tests

--- tests/run.rb
+++ tests/run.rb
@@ -8,12 +8,13 @@ def embed_in_layout(html)
 end
 
 start_time = Time.now
-puts 'Starting tests...'
+puts '🧪 Starting tests...'
 Dir.glob('tests/precompiled/**/*.html*').each do |file|
+  start_file_time = Time.now
   file_contents = embed_in_layout(File.read(file))
   compiled = BootstrapEmail::Compiler.new(type: :string, input: file_contents).perform_full_compile
   destination = file.sub('tests/precompiled/', '').sub('.erb', '')
   File.write("tests/compiled/#{destination}", compiled)
-  puts "Compiled #{destination}"
+  puts "🚀 Built #{destination} (in #{(Time.now - start_file_time).truncate(2)}s)"
 end
-puts "Finished compiling tests in #{(Time.now - start_time).truncate(2)}s!"
+puts "Finished compiling tests in #{(Time.now - start_time).truncate(2)}s 🎉"
