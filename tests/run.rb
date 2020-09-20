e09e109b0c320ac9087f2cf019b5fea9aab425c2
Stuart <stuart@yamartino.com>
2020-09-19 21:15:55 -0400

    Reorganize tests folder structure

--- tests/run.rb
+++ tests/run.rb
@@ -2,16 +2,16 @@ require_relative '../lib/bootstrap_email'
 require 'sassc'
 
 def embed_in_layout(html)
-  namespace = OpenStruct.new(contents: html)
+  namespace = OpenStruct.new(contents: ERB.new(html).result)
   template_html = File.read(File.expand_path('layout.html.erb', __dir__))
   ERB.new(template_html).result(namespace.instance_eval { binding })
 end
 
 puts 'Starting tests...'
-Dir.glob('tests/precompiled/*.html').each do |file|
+Dir.glob('tests/precompiled/**/*.html*').each do |file|
   file_contents = embed_in_layout(File.read(file))
   compiled = BootstrapEmail::Compiler.new(type: :string, input: file_contents).perform_full_compile
-  destination = file.split('/').last
+  destination = file.sub('tests/precompiled/', '')
   File.write("tests/compiled/#{destination}", compiled)
   puts "Compiled #{destination}"
 end
