ef024be37a02f178973650739f9343bc5966ce68
Stuart <stuart@yamartino.com>
2020-10-08 22:55:04 -0400

    rename test folder structure
    precompiled -> input
    compiled -> output

--- tests/run.rb
+++ tests/run.rb
@@ -2,12 +2,12 @@ require_relative '../lib/bootstrap_email'
 
 start_time = Time.now
 puts '🧪 Starting tests...'
-Dir.glob('tests/precompiled/**/*.html*').each do |file|
+Dir.glob('tests/input/**/*.html*').each do |file|
   start_file_time = Time.now
   file_contents = ERB.new(File.read(file)).result
   compiled = BootstrapEmail::Compiler.new(type: :string, input: file_contents).perform_full_compile
-  destination = file.sub('tests/precompiled/', '').sub('.erb', '')
-  File.write("tests/compiled/#{destination}", compiled)
+  destination = file.sub('tests/input/', '').sub('.erb', '')
+  File.write("tests/output/#{destination}", compiled)
   puts "🚀 Built #{destination} (in #{(Time.now - start_file_time).truncate(2)}s)"
 end
 puts "Finished compiling tests in #{(Time.now - start_time).truncate(2)}s 🎉"
