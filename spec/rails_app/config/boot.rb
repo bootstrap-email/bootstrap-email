706bdbe1883ba335eca9ab13b1a0372102aefd50
Stuart <stuart@yamartino.com>
2021-01-16 17:08:31 -0500

    Test for all 3 integration types

--- spec/rails_app/config/boot.rb
+++ spec/rails_app/config/boot.rb
@@ -2,6 +2,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../Gemfile', __dir__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+$LOAD_PATH.unshift File.expand_path('../../../lib', __dir__)
 require_relative '../../../lib/bootstrap_email'
-
-byebug
