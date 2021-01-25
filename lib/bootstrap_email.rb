64d95a695bf317df433bd2bfeef0324fcfec7181
Stuart <stuart@yamartino.com>
2021-01-25 15:30:38 -0500

    Load base component first

--- lib/bootstrap_email.rb
+++ lib/bootstrap_email.rb
@@ -18,7 +18,7 @@ require_relative 'bootstrap-email/initialize'
 require_relative 'bootstrap-email/compiler'
 require_relative 'bootstrap-email/sass_cache'
 require_relative 'bootstrap-email/version'
-
+require_relative 'bootstrap-email/components/base'
 Dir[File.join(__dir__, 'bootstrap-email/components', '*.rb')].each { |file| require_relative file }
 
 if defined?(Rails)
