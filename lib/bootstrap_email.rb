c8b429aa59800ccc6f56b56a779d3e2c8996907d
Stuart <stuart@yamartino.com>
2021-01-25 15:27:39 -0500

    Test

--- lib/bootstrap_email.rb
+++ lib/bootstrap_email.rb
@@ -18,6 +18,7 @@ require_relative 'bootstrap-email/initialize'
 require_relative 'bootstrap-email/compiler'
 require_relative 'bootstrap-email/sass_cache'
 require_relative 'bootstrap-email/version'
+
 Dir[File.join(__dir__, 'bootstrap-email/components', '*.rb')].each { |file| require_relative file }
 
 if defined?(Rails)
