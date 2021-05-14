b43af30e959e11bd2d0aca103dad1f16f01e6870
Stuart <stuart@yamartino.com>
2021-05-13 21:22:32 -0400

    Fix responsive padding support

--- lib/bootstrap-email/converters/padding.rb
+++ lib/bootstrap-email/converters/padding.rb
@@ -5,9 +5,9 @@ module BootstrapEmail
         each_node("*[class^=p-], *[class^=pt-], *[class^=pr-], *[class^=pb-], *[class^=pl-], *[class^=px-], *[class^=py-], *[class*=' p-'], *[class*=' pt-'], *[class*=' pr-'], *[class*=' pb-'], *[class*=' pl-'], *[class*=' px-'], *[class*=' py-']") do |node|
           next if ['table', 'td', 'a'].include?(node.name)
 
-          padding_regex = /(p[trblxy]?-\d+)/
-          classes = node['class'].scan(padding_regex).join(' ')
-          node['class'] = node['class'].gsub(padding_regex, '')
+          padding_regex = /(p[trblxy]?-(lg-)?\d+)/
+          classes = node['class'].gsub(padding_regex).to_a.join(' ')
+          node['class'] = node['class'].gsub(padding_regex, '').strip
           node.replace(template('table', classes: classes, contents: node.to_html))
         end
       end
