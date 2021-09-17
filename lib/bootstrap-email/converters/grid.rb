29cf843b34f0c48f0f1762630001d6dd72299e3b
Stuart Yamartino <stuart@yamartino.com>
2021-09-17 18:03:34 -0400

    Add a `row-responsive` to grids that are responsive (#128)
    This allows us to fix negative margins when grid cols are responsive.

--- lib/bootstrap-email/converters/grid.rb
+++ lib/bootstrap-email/converters/grid.rb
@@ -3,6 +3,9 @@ module BootstrapEmail
     class Grid < Base
       def build
         each_node('.row') do |node|
+          if node.at("./*[contains(@class, 'col-lg-')]")
+            add_class(node, 'row-responsive')
+          end
           node.replace(template('div', classes: node['class'], contents: template('table-to-tr', contents: node.inner_html)))
         end
         each_node('*[class*=col]') do |node|
