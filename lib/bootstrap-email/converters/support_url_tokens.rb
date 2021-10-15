0339ff72b8b908286d2bec5fd6eef6c6c1406f82
Stuart <stuart@yamartino.com>
2021-10-15 17:44:43 -0400

    Pull our open and close brackets into constants

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -1,8 +1,11 @@
 module BootstrapEmail
   module Converter
     class SupportUrlTokens < Base
+      OPEN_BRACKETS = URI.encode('{{').freeze
+      CLOSE_BRACKETS = URI.encode('}}').freeze
+
       def self.replace(html)
-        regex = /((href|src)=(\"|\'))((%7B%7B).*?(%7D%7D))(\"|\')/
+        regex = /((href|src)=(\"|\'))((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(\"|\')/
         if regex.match?(html)
           html.gsub!(regex) do |match|
             "#{$1}#{URI.decode($4)}#{$7}"
