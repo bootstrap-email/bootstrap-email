31c8430817ab7e9951d07c33712a63cc4842b13e
Stuart <stuart@yamartino.com>
2021-10-15 20:46:34 -0400

    Use CGI because URI encode is removed in ruby 3

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -1,14 +1,14 @@
 module BootstrapEmail
   module Converter
     class SupportUrlTokens < Base
-      OPEN_BRACKETS = URI.encode('{{').freeze
-      CLOSE_BRACKETS = URI.encode('}}').freeze
+      OPEN_BRACKETS = CGI.escape('{{').freeze
+      CLOSE_BRACKETS = CGI.escape('}}').freeze
 
       def self.replace(html)
         regex = /((href|src)=(\"|\'))((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(\"|\')/
         if regex.match?(html)
           html.gsub!(regex) do |match|
-            "#{$1}#{URI.decode($4)}#{$7}"
+            "#{$1}#{CGI.unescape($4)}#{$7}"
           end
         end
       end
