9beb6ce38341f425df4285fa6a1d2b5245428131
Stuart <stuart@yamartino.com>
2021-10-21 20:30:27 -0400

    replace html to change original reference

--- lib/bootstrap-email/converters/beautify_html.rb
+++ lib/bootstrap-email/converters/beautify_html.rb
@@ -2,7 +2,7 @@ module BootstrapEmail
   module Converter
     class BeautifyHTML < Base
       def self.replace(html)
-        html = HtmlBeautifier.beautify(html)
+        html.replace(HtmlBeautifier.beautify(html))
       end
     end
   end
