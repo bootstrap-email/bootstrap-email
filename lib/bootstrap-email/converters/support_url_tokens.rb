ebdd98d57ef2c758edd5d98de8b8210bd801ae1d
Stuart Yamartino <stuart@yamartino.com>
2022-03-30 16:57:33 -0400

    Add spec and support partial url tokens (#172)

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -5,7 +5,7 @@ module BootstrapEmail
       CLOSE_BRACKETS = CGI.escape('}}').freeze
 
       def self.replace(html)
-        regex = /((href|src)=(\"|\'))((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(\"|\')/
+        regex = /((href|src)=(\"|\').*?)((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(.*?(\"|\'))/
         if regex.match?(html)
           html.gsub!(regex) do |match|
             "#{$1}#{CGI.unescape($4)}#{$7}"
