483979a6fa97bc04828d792e69c309fac954a265
Stuart Yamartino <stuart@yamartino.com>
2022-04-30 08:57:52 -0400

    Switch to guard clause

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -8,10 +8,10 @@ module BootstrapEmail
 
       def self.replace(html)
         regex = /((href|src)=("|').*?)((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(.*?("|'))/
-        if regex.match?(html)
-          html.gsub!(regex) do |_match|
-            "#{Regexp.last_match(1)}#{CGI.unescape(Regexp.last_match(4))}#{Regexp.last_match(7)}"
-          end
+        return unless regex.match?(html)
+
+        html.gsub!(regex) do |_match|
+          "#{Regexp.last_match(1)}#{CGI.unescape(Regexp.last_match(4))}#{Regexp.last_match(7)}"
         end
       end
     end
