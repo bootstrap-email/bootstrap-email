b562495238bc8388ddf11f66d4c45498b576183f
Stuart Yamartino <stuart@yamartino.com>
2022-06-16 21:37:16 -0400

    Support multiple url tokens (#188)

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -7,11 +7,19 @@ module BootstrapEmail
       CLOSE_BRACKETS = CGI.escape('}}').freeze
 
       def self.replace(html)
-        regex = /((href|src)=("|').*?)((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))(.*?("|'))/
+        regex = /((href|src)=("|'))(.*?((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)})).*?)("|')/
         return unless regex.match?(html)
 
+        inner_regex = /((#{Regexp.quote(OPEN_BRACKETS)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}))/
+
         html.gsub!(regex) do |_match|
-          "#{Regexp.last_match(1)}#{CGI.unescape(Regexp.last_match(4))}#{Regexp.last_match(7)}"
+          start_text = Regexp.last_match(1)
+          middle_text = Regexp.last_match(4)
+          end_text = Regexp.last_match(8)
+          middle_text.gsub!(inner_regex) do |match|
+            CGI.unescape(match)
+          end
+          "#{start_text}#{middle_text}#{end_text}"
         end
       end
     end
