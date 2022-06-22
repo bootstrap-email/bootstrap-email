b76bd9a29e693625be1d4affba127e5bc2fd51de
Brook Sabin <basabin54@gmail.com>
2022-06-22 01:46:00 -0600

    use latest unicode characters from Litmuses updated guidelines

--- lib/bootstrap-email/converters/preview_text.rb
+++ lib/bootstrap-email/converters/preview_text.rb
@@ -8,7 +8,7 @@ module BootstrapEmail
         return if preview_node.nil?
 
         # apply spacing after the text max of 480 characters so it doesn't show body text
-        preview_node.inner_html += '&nbsp;&zwnj;' * [(480 - preview_node.content.length.to_i), 0].max
+        preview_node.inner_html += '&#847; &zwnj; &nbsp; ' * [(278 - preview_node.content.length.to_i), 0].max
         node = template('div', classes: 'preview', contents: preview_node.content)
         preview_node.remove
 
