8fb3eca1159d422f12e79714abb4552774e34017
Stuart Yamartino <stuart@yamartino.com>
2022-07-02 11:59:05 -0400

    Add link to litmus article

--- lib/bootstrap-email/converters/preview_text.rb
+++ lib/bootstrap-email/converters/preview_text.rb
@@ -7,6 +7,7 @@ module BootstrapEmail
         preview_node = doc.at_css('preview')
         return if preview_node.nil?
 
+        # https://www.litmus.com/blog/the-little-known-preview-text-hack-you-may-want-to-use-in-every-email/
         # apply spacing after the text max of 278 characters so it doesn't show body text
         preview_node.inner_html += '&#847; &zwnj; &nbsp; ' * [(278 - preview_node.content.length.to_i), 0].max
         node = template('div', classes: 'preview', contents: preview_node.content)
