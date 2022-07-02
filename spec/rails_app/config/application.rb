7e47b58786e6cd07e399a22730dbea9ccc99edea
Stuart Yamartino <stuart@yamartino.com>
2022-07-02 17:05:11 -0400

    Rails 7

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -4,7 +4,7 @@ require_relative 'boot'
 
 require 'action_mailer/railtie'
 require 'action_view/railtie'
-# require 'sprockets/railtie'
+require 'rails/railtie'
 require 'rails/test_unit/railtie'
 
 Bundler.require(*Rails.groups)
