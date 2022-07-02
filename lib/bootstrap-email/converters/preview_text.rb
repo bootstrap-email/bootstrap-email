c2303479d0d30b316a8d37f1a77b63726424249e
Brook Sabin <basabin54@gmail.com>
2022-07-02 09:57:02 -0600

    Preview Text Expansion/Update (#192)
    * use Litmus style of preview text padding and increase size
    
    * use latest unicode characters from Litmuses updated guidelines
    
    * fix comment text

--- lib/bootstrap-email/converters/preview_text.rb
+++ lib/bootstrap-email/converters/preview_text.rb
@@ -7,8 +7,8 @@ module BootstrapEmail
         preview_node = doc.at_css('preview')
         return if preview_node.nil?
 
-        # apply spacing after the text max of 100 characters so it doesn't show body text
-        preview_node.inner_html += '&nbsp;' * [(100 - preview_node.content.length.to_i), 0].max
+        # apply spacing after the text max of 278 characters so it doesn't show body text
+        preview_node.inner_html += '&#847; &zwnj; &nbsp; ' * [(278 - preview_node.content.length.to_i), 0].max
         node = template('div', classes: 'preview', contents: preview_node.content)
         preview_node.remove
 
