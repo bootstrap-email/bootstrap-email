a90598914b6c0bafb3bc7915600af8989df63b09
Hugo Da Roit <hugo@vertical-life.info>
2022-12-23 23:21:46 +0100

    fix race condition access on sass cache

---
+++ spec/sass_cache_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+RSpec.describe BootstrapEmail::SassCache do
+  describe '#compile' do
+    it 'is thread safe' do
+      config = BootstrapEmail::Config.new
+      SassC.load_paths << config.sass_load_paths
+
+      10.times do
+        threads = []
+        FileUtils.rm_rf(Dir.glob("#{config.sass_cache_location}/*"))
+
+        10.times do
+          threads << Thread.new do
+            css = BootstrapEmail::SassCache.compile('bootstrap-head', config)
+            expect(css.length).not_to eq(0)
+          end
+        end
+
+        threads.each(&:join)
+      end
+    end
+  end
+end
