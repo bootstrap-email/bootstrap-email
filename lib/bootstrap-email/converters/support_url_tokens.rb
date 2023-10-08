199e42761026bf644141460d8b6bf20fa26222ad
Stuart Yamartino <stuart@yamartino.com>
2023-10-07 22:11:22 -0400

    Fix post nokogiri upgrade

--- lib/bootstrap-email/converters/support_url_tokens.rb
+++ lib/bootstrap-email/converters/support_url_tokens.rb
@@ -3,21 +3,14 @@
 module BootstrapEmail
   module Converter
     class SupportUrlTokens < Base
-      OPEN_BRACKETS = CGI.escape('{{').freeze
-      OPEN_PERCENT = (CGI.escape('{') + '%').freeze
-      CLOSE_BRACKETS = CGI.escape('}}').freeze
-      CLOSE_PERCENT = ('%' + CGI.escape('}')).freeze
+      OPEN_BRACKETS = [CGI.escape('{{'), '{{', "#{CGI.escape('{')}%", '{%'].freeze
+      CLOSE_BRACKETS = [CGI.escape('}}'), '}}', "%#{CGI.escape('}')}", '%}'].freeze
 
       def self.replace(html)
-        regex = /((href|src)=("|'))(.*?((#{Regexp.quote(OPEN_BRACKETS)}|#{Regexp.quote(OPEN_PERCENT)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}|#{Regexp.quote(CLOSE_PERCENT)})).*?)("|')/
-        if regex.match?(html)
-          puts '*********** ITS A MATCH'
-        else
-          puts '*********** ITS NOT A MATCH'
-        end
+        regex = /((href|src)=("|'))(.*?((#{opening_regex}).*?(#{closing_regex})).*?)("|')/
         return unless regex.match?(html)
 
-        inner_regex = /((#{Regexp.quote(OPEN_BRACKETS)}|#{Regexp.quote(OPEN_PERCENT)}).*?(#{Regexp.quote(CLOSE_BRACKETS)}|#{Regexp.quote(CLOSE_PERCENT)}))/
+        inner_regex = /((#{opening_regex}).*?(#{closing_regex}))/
 
         html.gsub!(regex) do |_match|
           start_text = Regexp.last_match(1)
@@ -26,12 +19,16 @@ module BootstrapEmail
           middle_text.gsub!(inner_regex) do |match|
             CGI.unescape(match)
           end
-          puts 'Text pieces!!!!!!!!!!!!'
-          puts start_text
-          puts middle_text
-          puts end_text
           "#{start_text}#{middle_text}#{end_text}"
         end
+      end
+
+      def self.opening_regex
+        opening_regex = OPEN_BRACKETS.map { |bracket| Regexp.quote(bracket) }.join('|')
+      end
+
+      def self.closing_regex
+        closing_regex = CLOSE_BRACKETS.map { |bracket| Regexp.quote(bracket) }.join('|')
       end
     end
   end
