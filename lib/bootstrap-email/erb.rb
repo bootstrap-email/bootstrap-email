a000986c4ac69ecaa3b28ec14a0bfb5205469c19
Stuart Yamartino <stu@stuyam.com>
2025-10-06 19:30:24 -0400

    remove open struct dependency

--- lib/bootstrap-email/erb.rb
+++ lib/bootstrap-email/erb.rb
@@ -3,7 +3,7 @@
 module BootstrapEmail
   class Erb
     def self.template(path, locals_hash = {})
-      namespace = OpenStruct.new(locals_hash)
+      namespace = Struct.new(*locals_hash.keys, keyword_init: true).new(**locals_hash)
       template_html = File.read(path)
       ERB.new(template_html).result(namespace.instance_eval { binding })
     end
