0cc645dd1adb371121ee90759582e4ef361d567b
Stuart Yamartino <stu@stuyam.com>
2025-10-06 17:20:16 -0400

    Bug: Fix spacer class lookup (#283)
    Fixes #271

--- lib/bootstrap-email/converters/spacer.rb
+++ lib/bootstrap-email/converters/spacer.rb
@@ -5,6 +5,8 @@ module BootstrapEmail
     class Spacer < Base
       def build
         each_node('*[class*=s-]') do |node|
+          next unless node['class'].split.any? { |cls| cls.match?(/^s(-lg)?-\d+$/) }
+
           node.replace(template('table', classes: "#{node['class']} w-full", contents: '&nbsp;'))
         end
       end
