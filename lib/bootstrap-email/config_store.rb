14d60b6d4fa6eb2031fb8af2aa5e072b738141a7
Stuart Yamartino <stu@stuyam.com>
2025-12-11 14:25:29 -0500

    Comment fixes

--- lib/bootstrap-email/config_store.rb
+++ lib/bootstrap-email/config_store.rb
@@ -3,15 +3,15 @@
 module BootstrapEmail
   class ConfigStore
     OPTIONS = [
-      :sass_email_location,     # path to main sass file
-      :sass_head_location,      # path to head sass file
-      :sass_email_string,       # main sass file passed in as a string
-      :sass_head_string,        # head sass file passed in as a string
-      :sass_load_paths,         # array of directories for loading sass imports
-      :sass_cache_location,     # path to tmp folder for sass cache
-      :sass_log_enabled,        # turn on or off sass log when caching new sass
+      :sass_email_location,      # path to main sass file
+      :sass_head_location,       # path to head sass file
+      :sass_email_string,        # main sass file passed in as a string
+      :sass_head_string,         # head sass file passed in as a string
+      :sass_load_paths,          # array of directories for loading sass imports
+      :sass_cache_location,      # path to tmp folder for sass cache
+      :sass_log_enabled,         # turn on or off sass log when caching new sass
       :generate_rails_text_part, # boolean for whether or not to generate the text part in rails, default: true
-      :override_mail_method # method to override the mail method in rails, default: false
+      :override_mail_method      # boolean to override the mail method in rails, default: false
     ].freeze
 
     attr_accessor(*OPTIONS)
