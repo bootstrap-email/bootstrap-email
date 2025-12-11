84df990d6990c5b548541b2c3841547a3e2bc79f
Stuart Yamartino <stu@stuyam.com>
2025-12-11 14:11:54 -0500

    Update action_mailer.rb

--- lib/bootstrap-email/rails/action_mailer.rb
+++ lib/bootstrap-email/rails/action_mailer.rb
@@ -4,14 +4,14 @@ ActiveSupport.on_load(:action_mailer, { yield: true }) do |action_mailer|
   action_mailer.class_eval do # To support Rails less than 6
     # Only override mail method if override_mail_method is enabled
     if BootstrapEmail.static_config.override_mail_method
-      alias_method :_rails_mail, :mail unless method_defined?(:_rails_mail)
+      alias_method :rails_mail, :mail unless method_defined?(:rails_mail)
       def mail(*args, &block)
         bootstrap_mail(*args, &block)
       end
     end
 
     def bootstrap_mail(*args, &block)
-      message = method_defined?(:_rails_mail) ? _rails_mail(*args, &block) : mail(*args, &block)
+      message = method_defined?(:rails_mail) ? rails_mail(*args, &block) : mail(*args, &block)
       BootstrapEmail::Rails::MailBuilder.perform(message)
     end
     alias_method :bootstrap_email, :bootstrap_mail
